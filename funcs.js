8da9a9325cacd2e717dd7965ba4b2860b5defc36
aibvera <alejandro.bueno.vera.97@gmail.com>
2024-05-14 23:39:56 -0500

    first commit

---
+++ funcs.js
@@ -0,0 +1,21 @@
+function moveTo(tagName) {
+
+    document.getElementById(tagName).scrollIntoView();
+
+}
+
+function addRecomm() {
+
+    var newName = document.createElement('p');
+    var newText = document.createElement('p');
+    var newRecomm = document.createElement('div');
+    newName.innerHTML = document.getElementById("name").value;
+    newText.innerHTML = document.getElementById("text").value;
+    newName.className = "recommName";
+    newText.className = "recommText";
+    newRecomm.className = "recomm";
+    newRecomm.appendChild(newText);
+    newRecomm.appendChild(newName);
+    document.getElementById("recomms").appendChild(newRecomm);
+
+}
